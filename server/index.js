50d8a1d951a3d7be34f1bbfde2dbebccaf065cfd
Shubham Vishwakarma <skv6621@gmail.com>
2024-11-13 20:20:34 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://vikas-quiz-rqt4.vercel.app/"],
+    origin: ["https://vikas-quiz-rqt4.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }
